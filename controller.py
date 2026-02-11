08c71c51f215fc7abf564ae6b34e3aa5298c9094
JakeBodish <jab130@uark.edu>
2026-02-10 23:53:12 -0600

    Sprint2. Update controller

--- controller.py
+++ controller.py
@@ -2,33 +2,32 @@
 # Class handles communication between hardware and server and keyboard inputs
 ######
 import socket
-import select
 from time import sleep
 import queue
 import pygame
 from pygame.locals import*
 import threading
-from sys import exit
 
 class Controller():
 	def __init__(self):
 		self.running  = True
-		self.localIP     = "0.0.0.0"
-		self.localPort   = 7501
-		self.remotePort = 7500
+		self.serverIP = "0.0.0.0"
+		self.incomingPort = 7501
+		self.outgoingPort = 7500
 		self.bufferSize  = 1024
-		self.UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-		self.UDPServerSocket.bind((self.localIP, self.localPort)) # Bind server socket to an IP
+
+		# Sockets
+		self.UDPIncomingSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+		self.UDPIncomingSocket.bind((self.serverIP, self.incomingPort)) # Bind incoming socket to an IP
+	
+		self.UDPOutgoingSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+		self.UDPOutgoingSocket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1) # Enable broadcast on outgoing socket
 		
 		# Thread to constantly listen to data and then store in data buffer.
 		self.data_in_buffer = queue.Queue()
-		self.databuffer_lock = threading.Lock()
-		# self.listner = threading.Thread(target=self.listen(), args=(self), daemon=True)
-		# self.listner.start()
-		
+		self.listner = threading.Thread(target=self.listen, args=(), daemon=True)
 	
 	def update(self):
-		self.listen()
 		for event in pygame.event.get(): # Process any key inputs
 			if event.type == QUIT:
 				self.running = False
@@ -36,41 +35,32 @@ class Controller():
 				if event.key == pygame.K_ESCAPE:
 					self.running = False
 	
-	def send_data(self, data, clientIP): # send data to a client
+	def broadcast(self, data): # send data to a client
 		bytes_to_send = str.encode(data)
-		self.UDPServerSocket.sendto(bytes_to_send, clientIP)
-	
+		self.UDPOutgoingSocket.sendto(bytes_to_send, ("255.255.255.255", self.outgoingPort)) # Broadcast to all devices on the network
+# START GAME
 	def start(self): # Start code is 202
-		''' 
-		for player in player_database:
-			self.send_data("202", (player.ip, self.remotePort))
-		'''
-		self.send_data("202", ("127.0.0.1", self.remotePort)) # coded for one client right now (trafficgenerator.py)
-	
+		self.broadcast("202")
+		self.listner.start() 
+# END GAME
 	def end(self): # End code is 221
-		''' 
-		for player in player_database:
-			self.send_data("221", (player.ip, self.remotePort))
-		'''
-		self.send_data("221", ("127.0.0.1", self.remotePort)) # coded for one client right now (trafficgenerator.py)
-		
-	def listen(self): # Listen for new data
-		ready_to_read, _, _ = select.select([self.UDPServerSocket], [], [], 1)
-		if ready_to_read:
+		self.broadcast("221") # Broadcast end code to all clients on the network
+
+
+	def listen(self): # Worker function for listner thread
+		while self.running:
 			try:
-				self.data_in_buffer.put(self.UDPServerSocket.recvfrom(self.bufferSize))
+				self.data_in_buffer.put(self.UDPIncomingSocket.recvfrom(self.bufferSize))
 			except socket.error as e:
 				print(f"Socket error: {e}")
-		sleep(0.04)
-		# have an input and output buffer for sending and recieving data
-		# have a thread running to see if it can read or write constantly. based on if there is data in buffers or not
-		
-	# Functions to update network parameters
-	def change_localIP(self, new_ip):
-		self.localIP = new_ip
-		self.UDPServerSocket.bind((self.localIP, self.localPort))
-	def change_localPort(self, new_port):
-		self.localPort= new_port
-		self.UDPServer((self.localIP, self.localPort))
-	def change_remotePort(self, new_port):
-		self.remotePort = new_port
+			sleep(0.04)
+
+# Functions to update network parameters
+	def change_serverIP(self, new_ip): # This is the one for Sprint 2. Others are extra
+		self.serverIP = new_ip
+		self.UDPIncomingSocket.bind((self.serverIP, self.incomingPort))
+	def change_incomingPort(self, new_port):
+		self.incomingPort= new_port
+		self.UDPIncomingSocket.bind((self.serverIP, self.incomingPort))
+	def change_outgoingPort(self, new_port):
+		self.outgoingPort = new_port
