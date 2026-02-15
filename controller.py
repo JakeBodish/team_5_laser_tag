8508ee29f10aa2c0abe9cb3c6c8dbb9e1acf7ad0
alexhayek <alexhayek28@gmail.com>
2026-02-14 21:17:20 -0600

    Update controller.py

--- controller.py
+++ controller.py
@@ -2,7 +2,6 @@
 # Class handles communication between hardware and server and keyboard inputs
 ######
 import socket
-from time import sleep
 import queue
 import pygame
 from pygame.locals import*
@@ -11,6 +10,7 @@ import threading
 class Controller():
 	def __init__(self):
 		self.running  = True
+		#true when game running
 		self.in_progress = False
 		
 		# Network configuration
@@ -20,101 +20,114 @@ class Controller():
 		self.bufferSize  = 1024
 
 		# Sockets
+		#incoming socket
 		self.UDPIncomingSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 		self.UDPIncomingSocket.bind((self.serverIP, self.incomingPort)) # Bind incoming socket to an IP
-	
+
+		#outgoing socket
 		self.UDPOutgoingSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 		self.UDPOutgoingSocket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1) # Enable broadcast on outgoing socket
 		
 		# Thread to constantly listen to data and then store in data buffer.
-		self.data_in_buffer = None # Initialized on game start
-		self.listner = threading.Thread(target=self.listen, args=(), daemon=True)
+		self.data_in_buffer = queue.Queue()
+		self.stop_event = threading.Event()
+		self.listener = threading.Thread(target=self.listen, daemon=True)
 
-# KEY INPUTS	
-	def update(self):
-		for event in pygame.event.get(): 
-			if event.type == QUIT:
-				self.running = False
-			if event.type == pygame.KEYDOWN:
-				if event.key == pygame.K_ESCAPE:
-					self.running = False
-			elif event.type == pygame.MOUSEBUTTONUP: # Mouse clicked
-				continue; 
-			elif event.type == pygame.KEYUP: # Key released; put hotkeys here
-				match event.key:
-					
-					case pygame.K_F1: # Return to Entry screen
-						# TODO: Signal switch back to Entry screen
-						# TODO: Signal to 
-						continue;
-						
-					case pygame.K_F2: # Configure network during Entry 
-						newIP = None #TODO: Prompt user for new server IP
-						self.change_serverIP(newIP)
-						
-					case pygame.K_F3: # Start Game from Entry Screen
-						if not self.in_progress:
-							self.start()
-						# TODO: Send start signal to view to switch screens
-	
-					case pygame.K_F12: # Wipe players 
-						if self.in_progress:
-							self.end()
-						else:
-							# TODO: Signal model to wipe if on entry
-							continue;
-						
-					case pygame.K_TAB: # Swtich current entry field
-						if not self.in_progress:
-							# TODO: Switch field
-						continue;
-						
-					case pygame.K_DELETE: # Delete selected player from Entry Screen
-						if not self.in_progress:
-							# TODO: Delete selected player
-						# TODO: Signal model to delete player
-						continue;
-			
-	def broadcast(self, data): # broadcast data to all clients
-		bytes_to_send = str.encode(data)
-		self.UDPOutgoingSocket.sendto(bytes_to_send, ("255.255.255.255", self.outgoingPort)) # Broadcast to all devices on the network
+		#input flags
+		self.request_start = False
+		self.request_add = False
+		self.request_delete = False
+		self.request_wipe = False
+
+		#IP change mode
+		self.ip_mode = False
+		self.ip_text = ""
+
+#event processing	
+	def process_events(self, events):
+        for event in events:
+            if event.type == QUIT:
+                self.running = False
+            if event.type == KEYDOWN:
+                #esc quits program or cancels IP input
+                if event.key == K_ESCAPE:
+                    if self.ip_mode:
+                        self.ip_mode = False
+                    else:
+                        self.running = False
+
+                #if typing IP address
+                if self.ip_mode:
+                    if event.key == K_RETURN:
+                        self.change_serverIP(self.ip_text)
+                        self.ip_mode = False
+                    elif event.key == K_BACKSPACE:
+                        self.ip_text = self.ip_text[:-1]
+                    else:
+                        if event.unicode.isdigit() or event.unicode == ".":
+                            self.ip_text += event.unicode
+
+            if event.type == KEYUP:
+                if event.key == K_F2:
+                    self.ip_mode = True
+                    self.ip_text = ""
+
+                if event.key == K_F3:
+                    self.request_start = True
+
+                if event.key == K_F12:
+                    self.request_wipe = True
+
+                if event.key == K_a:
+                    self.request_add = True
+
+                if event.key == K_DELETE:
+                    self.request_delete = True
 		
-# START GAME
-	def start(self): # Start code is 202
-		self.broadcast("202")
-		self.listner.start()
-		self.in_progress = True
-		self.data_in_buffer = Queue.queue()
-# END GAME
-	def end(self): # End code is 221
-		self.broadcast("221") # Broadcast end code to all clients on the network
-		self.in_progress = False
-		self.data_in_buffer.shutdown()
+	#udp functions
+    def broadcast(self, msg):
+        self.UDPOutgoingSocket.sendto(
+            msg.encode(),
+            ("255.255.255.255", self.outgoingPort),
+        )
 
-###########################
-#
-# LISTENER THREAD STARTS AND ENDS WITH GAME START AND END
-#
-# TO RETRIEVE DATA, USE CONTROLLER.DATA_IN_BUFFER.GET()
-#
-##########################
-	def listen(self): # Worker function for listner thread
-		while self.in_progress:
-			try:
-				self.data_in_buffer.put(self.UDPIncomingSocket.recvfrom(self.bufferSize))
-			except socket.error as e:
-				print(f"Socket error: {e}")
-			except queue.Shutdown as e:
-				continue;
-			sleep(0.04)
+    def broadcast_equipment(self, equipment_id):
+        #broadcast equipment code after adds player
+        self.broadcast(f"EQ:{equipment_id}")
 
+    def start(self):
+        #broadcast game start code
+        self.broadcast("202")
+        self.in_progress = True
 
-# Functions to update network parameters
-	def change_serverIP(self, new_ip): # This is the one for Sprint 2. Others are extra
-		self.serverIP = new_ip
-		self.UDPIncomingSocket.bind((self.serverIP, self.incomingPort))
-	def change_incomingPort(self, new_port):
-		self.incomingPort= new_port
-		self.UDPIncomingSocket.bind((self.serverIP, self.incomingPort))
-	def change_outgoingPort(self, new_port):
-		self.outgoingPort = new_port
+        self.stop_event.clear()
+        if not self.listener.is_alive():
+            self.listener = threading.Thread(target=self.listen, daemon=True)
+            self.listener.start()
+
+    def end(self):
+        #broadcast game end code
+        self.broadcast("221")
+        self.in_progress = False
+        self.stop_event.set()
+
+    def listen(self):
+        #thread listens for udp sockets
+        while not self.stop_event.is_set():
+            try:
+                data, addr = self.UDPIncomingSocket.recvfrom(self.bufferSize)
+                self.data_in_buffer.put((data, addr))
+            except:
+                break
+
+	#network configuration
+    def change_serverIP(self, new_ip):
+        #allows user to change network binding address
+        try:
+            self.serverIP = new_ip
+            self.UDPIncomingSocket.close()
+            self.UDPIncomingSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            self.UDPIncomingSocket.bind((self.serverIP, self.incomingPort))
+        except:
+            pass
+
