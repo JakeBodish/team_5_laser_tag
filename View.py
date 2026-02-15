5945e16562beca0a7be30290d3c23d9c4cc77787
alexhayek <alexhayek28@gmail.com>
2026-02-14 21:19:51 -0600

    Update View.py

--- View.py
+++ View.py
@@ -1,71 +1,52 @@
 import pygame
-from pygame.locals import*
-from time import sleep
+from pygame.locals import *
 
-class Screen():
-    SCREEN_SIZE = (800,800)
+class Screen:
+    SCREEN_SIZE = (800, 800)
+
     def __init__(self):
-        self.screen = pygame.display.set_mode(Screen.SCREEN_SIZE,)
+        self.screen = pygame.display.set_mode(Screen.SCREEN_SIZE)
+        #starts splash screen timer
         self.entry_screen = False
-        self.red_team_entry = []
-        self.green_team_entry = []
-        self.entry_text = []
-        self.entry_rows = 20
-        self.entry_col = 2
-        self.entry_box_height = 25
-        self.init_entry_arrays()
-        self.init_entry_text()
-        self.img = pygame.image.load('splash_screen.jpg')
-        self.scaled_img = pygame.transform.scale(self.img, (800, 800))
+        self.start_time = pygame.time.get_ticks()
+        #loads splash screen image
+        self.img = pygame.image.load("splash_screen.jpg")
+        self.img = pygame.transform.scale(self.img, Screen.SCREEN_SIZE)
+        #entry data storage
+        self.red_players = [{"id": "", "name": ""} for _ in range(20)]
+        self.green_players = [{"id": "", "name": ""} for _ in range(20)]
+        #current selection
+        self.team = "RED"
+        self.row = 0
+        self.col = 0
+        self.font = pygame.font.SysFont(None, 24)
 
-    def init_entry_arrays(self): # initalizes the red and green team entry screen arrays
-         for i in range(self.entry_rows):
-            for j in range(self.entry_col):
-                if(j == 0): #First box for player ID
-                    red_rect = pygame.Rect(100, 100 + self.entry_box_height * i, 75, self.entry_box_height)
-                    green_rect = pygame.Rect(500, 100 + self.entry_box_height * i, 75, self.entry_box_height)
-                if(j == 1): #Second box for player Name
-                    red_rect = pygame.Rect(175, 100 + self.entry_box_height * i, 125, self.entry_box_height)
-                    green_rect = pygame.Rect(575, 100 + self.entry_box_height * i, 125, self.entry_box_height)
-                
-                self.red_team_entry.append(red_rect)
-                self.green_team_entry.append(green_rect)
-
-    def init_entry_text(self):
-        # "Player Entry Screen" 
-        font = pygame.font.SysFont(None, 36)
-        text_surface = font.render("Player Entry Screen", True, (255, 255, 255))
-        text = (text_surface, (280, 25))
-        self.entry_text.append(text)
-
-        # "Red Team"
-        font = pygame.font.SysFont(None, 20)
-        text_surface = font.render("Red Team", True, (255, 0, 0))
-        text = (text_surface, (150, 75))
-        self.entry_text.append(text)
-
-        # "Green Team"
-        font = pygame.font.SysFont(None, 20)
-        text_surface = font.render("Green Team", True, (0, 255, 0))
-        text = (text_surface, (550, 75))
-        self.entry_text.append(text)
-
-
+    #draw screen
     def update(self):
         self.screen.fill((0, 0, 0))
-        if(self.entry_screen):
-            for rect in self.red_team_entry: # Draw red team entry table
-                pygame.draw.rect(self.screen, (255, 0, 0), rect, 2)
-            for rect in self.green_team_entry: # Draw green team entry table
-                pygame.draw.rect(self.screen, (0, 255, 0), rect, 2)
 
-            #Print words to the screen
-            for text in self.entry_text:
-                self.screen.blit(text[0], text[1])
-        else: 
-            #Prints scaled Splash Screen image, waits 3s and updates entry_screen
-            self.screen.blit(self.scaled_img, (0, 0))
-            sleep(3)
-            self.entry_screen = True
+        #splash screen for first 3 seconds
+        if not self.entry_screen:
+            self.screen.blit(self.img, (0, 0))
+            if pygame.time.get_ticks() - self.start_time > 3000:
+                self.entry_screen = True
+            pygame.display.flip()
+            return
 
+        #sraw entry screen
+        self.draw_entries()
         pygame.display.flip()
+
+    def draw_entries(self):
+        y = 100
+
+        for i in range(20):
+            #red side
+            pygame.draw.rect(self.screen, (255, 0, 0), (100, y, 75, 25), 2)
+            pygame.draw.rect(self.screen, (255, 0, 0), (175, y, 125, 25), 2)
+
+            #green side
+            pygame.draw.rect(self.screen, (0, 255, 0), (500, y, 75, 25), 2)
+            pygame.draw.rect(self.screen, (0, 255, 0), (575, y, 125, 25), 2)
+
+            y += 25
