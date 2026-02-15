fd50e552b14c9f430ba0547a7286910ce6992fa1
alexhayek <alexhayek28@gmail.com>
2026-02-14 21:21:28 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,37 @@
-######
-# Main program loop
-######
 import pygame
-from pygame.locals import*
-from time import sleep
 from controller import Controller
 from view import Screen
+from model import Model
 
-if __name__=="__main__":
-	pygame.init()
-	view = Screen()
-	controller = Controller()
+def main():
+    pygame.init()
+    view = Screen()
+    controller = Controller()
+    model = Model()
+    clock = pygame.time.Clock()
 
-	while controller.running:
-		view.update()
-		controller.update()
-		sleep(0.04)
+    while controller.running:
+        events = pygame.event.get()
+        controller.process_events(events)
+        #start game
+        if controller.request_start:
+            controller.start()
+            controller.request_start = False
+        #add player
+        if controller.request_add:
+            controller.request_add = False
+            model.add_player(1, "Test", "RED")
+            controller.broadcast_equipment(1)
+        #wipe database
+        if controller.request_wipe:
+            controller.request_wipe = False
+            controller.end()
+            model.wipe_all()
+
+        view.update()
+        clock.tick(25)
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
